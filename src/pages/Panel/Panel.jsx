ba6eb044089392df4f9821c96c2244076b9339e7
αѕнкαη <as6kan.developer@gmail.com>
2024-04-26 01:45:09 +0330

    add: auto hide scroll when change

--- src/pages/Panel/Panel.jsx
+++ src/pages/Panel/Panel.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Panel.css';
 import SideBar from './SideBar/SideBar';
 import TopBar from './TopBar/TopBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 
 export default function Panel() {
   const [isShowSideBar, setIsShowBar] = useState(true);
+  const URL = useParams();
+
+  useEffect(() => {
+    setIsShowBar(false);
+  }, [URL]);
+
   return (
     <div className='panel'>
       <div className={isShowSideBar ? 'sideBar' : 'sideBar hide'}>
