feeb5a0a97696e0f6278b372c5e2b2cf031a70c1
αѕнкαη <as6kan.developer@gmail.com>
2024-10-22 19:00:24 +0330

    fix trade traslate section

--- template/home/tradingSection/TradingTranslateSection.tsx
+++ template/home/tradingSection/TradingTranslateSection.tsx
@@ -1,11 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import { Tabs, Tab, Box, Button, TabsProps, TabProps } from "@mui/material";
 import LogoSvg from "./LogoSvg";
 import styles from "./TradingSection.module.css"; // ایمپورت استایل‌های مدولار
 import InputBase from "@/shared/components/UI/input/InputBase";
 import goldPrices from "@/data/goldPrices.json";
+import convertToPersianDigits from "@/shared/utilities/convertToPersianDigits";
+import convertToEnglishDigits from "@/shared/utilities/convertToEnglishDigits";
+import BigNumber from "bignumber.js";
+import Link from "next/link";
+import { useAuth } from "@/shared/hooks/useAuth";
 
 const StyledTabs = styled((props: TabsProps) => (
   <Tabs
@@ -41,12 +46,54 @@ const StyledTab = styled((props: TabProps) => <Tab disableRipple {...props} />)(
   }),
 );
 
+const convertToEnglishDigitsGold = (persianNumber: string) => {
+  const cleanedValue = persianNumber.replace(/[^۰-۹0-9.]/g, "");
+  const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
+  const englishDigits = "0123456789";
+
+  return cleanedValue.replace(/[۰-۹]/g, (digit) => {
+    return englishDigits[persianDigits.indexOf(digit)] || digit;
+  });
+};
+
 const TradingTranslateSection = () => {
+  const { isLoggedIn } = useAuth();
   const [tab, setTab] = useState(0);
-  const [tradeAction, setTradeAction] = useState("buy");
-  const [sumTotal, setSumTotal] = useState("");
-  const [sumTotalGold, setSumTotalGold] = useState("");
-  const [wichFocus, setWichFocus] = useState("price");
+  const [amountCash, setAmountCash] = useState("");
+  const [amountGold, setAmountGold] = useState("");
+
+  const handleCashChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCash = convertToEnglishDigits(e.target.value);
+    setAmountCash(newCash);
+    updateGoldFromCash(newCash);
+  };
+
+  const handleGoldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newGold = convertToEnglishDigitsGold(e.target.value);
+    setAmountGold(newGold);
+    updateCashFromGold(newGold);
+  };
+
+  const updateGoldFromCash = (cash: string) => {
+    const pricePerGram = new BigNumber(
+      tab === 0 ? goldPrices.buy : goldPrices.sell,
+    );
+    const newGold = new BigNumber(cash).div(pricePerGram).toFixed(3);
+    setAmountGold(newGold);
+  };
+
+  const updateCashFromGold = (gold: string) => {
+    const pricePerGram = new BigNumber(
+      tab === 0 ? goldPrices.buy : goldPrices.sell,
+    );
+    const newCash = new BigNumber(gold).times(pricePerGram).toFixed(0);
+    setAmountCash(newCash);
+  };
+
+  useEffect(() => {
+    setAmountCash("");
+    setAmountGold("");
+  }, [tab]);
 
   return (
     <>
@@ -82,45 +129,49 @@ const TradingTranslateSection = () => {
 
         <div className={styles.tradingBoxLeftInputs}>
           <InputBase
-            value={sumTotal}
-            onChange={(e) => setSumTotal(e.target.value)}
             label={"ارزش کل"}
+            value={convertToPersianDigits(amountCash)}
+            onChange={handleCashChange}
             name={"تومان"}
             style={{
+              marginTop: 12,
               width: "100%",
-              marginTop: 10,
-              backgroundColor: "#373943",
+              background: "rgb(55, 57, 67)",
               borderRadius: 16,
             }}
-          />{" "}
+            max={10}
+          />
           <InputBase
-            value={sumTotal}
-            onChange={(e) => setSumTotal(e.target.value)}
-            name={"گرم"}
             label={"مقدار طلا"}
+            value={convertToPersianDigits(amountGold)}
+            onChange={handleGoldChange}
+            name="گرم"
             style={{
+              marginTop: 12,
               width: "100%",
-              marginTop: 10,
-              backgroundColor: "#373943",
+              background: "rgb(55, 57, 67)",
               borderRadius: 16,
             }}
+            max={6}
           />
         </div>
       </div>
 
       <div className={styles.tradingBoxLeftBtn}>
-        <Button
-          style={{
-            border: "3px solid #f1ab1fa5",
-            borderRadius: 8,
-            color: "var(--primary-color)",
-          }}
-          size="large"
-          fullWidth
-          variant="outlined"
-        >
-          {tab ? "فروش" : "خرید"}
-        </Button>
+        <Link href={isLoggedIn ? "/trade" : "/login"}>
+          <Button
+            style={{
+              border: "3px solid #f1ab1fa5",
+              borderRadius: 8,
+              color: "var(--primary-color)",
+            }}
+            size="large"
+            fullWidth
+            variant="outlined"
+          >
+            {tab ? "فروش" : "خرید"}
+          </Button>
+        </Link>
       </div>
     </>
   );
