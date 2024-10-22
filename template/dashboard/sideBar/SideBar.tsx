cd75d60927eda5ce1d3b26d20bb2fd05cb1c2a0e
αѕнкαη <as6kan.developer@gmail.com>
2024-10-22 19:19:27 +0330

    fix(dashboard): logout

--- template/dashboard/sideBar/SideBar.tsx
+++ template/dashboard/sideBar/SideBar.tsx
@@ -14,6 +14,8 @@ import LogoSVG from "../svg/SideBarLogoSVG";
 import { usePathname } from "next/navigation";
 import { useSidebar } from "@/context/DashboardSideBarContext";
 import toast from "react-hot-toast";
+import axios from "axios";
+import { useAuth } from "@/shared/hooks/useAuth";
 
 interface SideBarItemProps {
   name: string;
@@ -36,14 +38,21 @@ const SideBarItem: React.FC<SideBarItemProps> = ({ name, icon, to }) => {
 };
 
 const SideBar: React.FC = () => {
+  const { mutate } = useAuth();
   const { isSidebarOpen } = useSidebar();
 
-  const signOutHandler = () => {
+  const signOutHandler = async () => {
     localStorage.removeItem("auth_token");
+    try {
+      const req = await axios.get("/api/auth/logout");
 
-    window.location.reload();
-
-    toast.success("خروج با موفقیت انجام شد.");
+      if (req.data.success) {
+        await mutate(null);
+        toast.success("خروج با موفقیت انجام شد.");
+      }
+    } catch (error) {
+      console.log("log out err", error);
+    }
   };
 
   return (
