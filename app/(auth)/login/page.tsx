efb112f39080245da0b52b9f607e0425a44b70ef
αѕнкαη <as6kan.developer@gmail.com>
2024-10-27 20:16:24 +0330

    fix(login): add default value

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,8 +8,8 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("as6kan.developer@gmail.com");
+  const [password, setPassword] = useState("goldika.test");
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
