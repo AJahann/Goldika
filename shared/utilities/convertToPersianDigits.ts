c0df762710f311635c2940f37486eaca8d81189e
αѕнкαη <as6kan.developer@gmail.com>
2024-10-28 23:32:08 +0330

    refactor(utils): NaN

--- shared/utilities/convertToPersianDigits.ts
+++ shared/utilities/convertToPersianDigits.ts
@@ -1,13 +1,12 @@
 const convertToPersianDigits = (num: string) => {
+  if (num === "NaN") return "";
+
   const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
   const replacedNumber = num.replace(
     /\d/g,
     (digit: string) => persianDigits[parseInt(digit)],
   );
 
-  if (replacedNumber === "NaN") {
-    return "";
-  }
   return replacedNumber;
 };
 
