c0c69298e6b1c6f60e30a23bc8196f9e6813798c
αѕнкαη <as6kan.developer@gmail.com>
2024-11-01 13:58:41 +0330

    add path ckeck webpack plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import CaseSensitivePathsPlugin from "case-sensitive-paths-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: ".next",
+  webpack: (config) => {
+    // اضافه کردن پلاگین به تنظیمات Webpack
+    config.plugins.push(new CaseSensitivePathsPlugin());
+    return config;
+  },
 };
 
 export default nextConfig;
