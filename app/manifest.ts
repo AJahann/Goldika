04b0d727d67b4e92a4939f9839a9dc368e2389cf
αѕнкαη <as6kan.developer@gmail.com>
2024-12-11 03:52:08 +0330

    fix: manifest icons [2x]

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,17 +12,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/logo.png",
+        src: "/logo192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/logo.png",
+        src: "/logo512.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "/logo.png",
+        src: "/logo1024.png",
         sizes: "1024x1024",
         type: "image/png",
       },
